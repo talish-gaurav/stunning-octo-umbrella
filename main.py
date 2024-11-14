4bf787d592238f204196febbc6abedbba721c959
talish-gaurav <97028189+talish-gaurav@users.noreply.github.com>
2024-11-14 19:06:01 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+
+n = int(input("Enter your number here"))
+p = int(input("Enter the power you would like your number up to"))
+
+for i in range(0,p,+1):
+
+    print(n,"to the power of" ,p ,"=" , i)
